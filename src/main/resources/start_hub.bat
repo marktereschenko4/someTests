030cb86afa4987734d8df0ac3070e9fdb79bf8d7
Mark Tereschenko4 <marktereschenko4@gmail.com>
2019-07-02 11:59:07 +0300

    second

---
+++ src/main/resources/start_hub.bat
@@ -0,0 +1,4 @@
+c:
+cd \Users\Mark\Downloads\grid\
+java -jar selenium-server-standalone-3.141.59.jar -role hub
+pause
---
+++ src/main/resources/start_node.bat
@@ -0,0 +1,4 @@
+c:
+cd \Users\Mark\Downloads\grid\
+java -jar selenium-server-standalone-3.141.59.jar -role node -hub http://192.168.0.2:4444/grid/register/ -browser browserName=firefox
+pause
